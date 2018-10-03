41cdb0de26cb2630753e2fdfc80b54b4fb808912
aderito.xavier <aderito_xavier@sapo.pt>
2018-10-03 21:54:34 +0100

    models added in the views

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Webpage, Topic
 
 # Create your views here.
 def index(request):
-    my_dict = {'body': "templating"}
+    access_records = AccessRecord.objects.all
+    my_dict = {'access_records': access_records}
     return render(request, 'first_app/index.html', context=my_dict)
 
 
