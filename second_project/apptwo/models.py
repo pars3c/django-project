8f5e32aa19e29c3819dce4b48e282d18541cd2c6
aderito.xavier <aderito_xavier@sapo.pt>
2018-10-06 22:06:45 +0100

    apptwo models added

--- second_project/apptwo/models.py
+++ second_project/apptwo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
--- second_project/apptwo/views.py
+++ second_project/apptwo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apptwo.models import Car
 
 # Create your views here.
 def index(request):
+    carros = Car.objects.all
+    cars = {'carros': carros}
     my_dict = {'super': "duper"}
-    return render(request, 'apptwo/index.html', context=my_dict)
+    return render(request, 'apptwo/index.html', context=cars)
