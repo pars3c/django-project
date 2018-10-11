16ad9f0c87e2cbb2062a3b844cd663dbc3e76ba7
aderito.xavier <aderito_xavier@sapo.pt>
2018-10-11 22:27:16 +0100

    Basic url patterns created

--- startforms/basicapp/views.py
+++ startforms/basicapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
