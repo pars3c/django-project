34ea0e21631123ad363952037562359d049804b6
aderito.xavier <aderito_xavier@sapo.pt>
2018-10-11 22:40:25 +0100

    template added to the forms project

--- startforms/basicapp/views.py
+++ startforms/basicapp/views.py
@@ -4,7 +4,5 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render(request, 'basicapp/index.html')
 
