316fd7519a58f8d754d262f84819e27ee1d327c0
aderito xavier <aderito_xavier@sapo.pt>
2018-10-21 22:18:40 +0100

    model forms added

--- pools/profiles/forms.py
+++ pools/profiles/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-
-class RegistrationForm(forms.Form):
-    name = forms.CharField(min_length=2, max_length=100)
-    age = forms.IntegerField(min_value=18, max_value=99)
-    CHOICES = (('Option 1', 'Option 3'),('Option 2', 'Option 2'),)
-    field = forms.ChoiceField(choices=CHOICES)
+from django.core import validators
+from profiles.models import UserProfile
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
--- pools/profiles/views.py
+++ pools/profiles/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from .forms import RegistrationForm
+from profiles.forms import UserForm
 # Create your views here.
 def home(request):
-    form = RegistrationForm()
+    form = UserForm()
+    if request.method == 'POST':
+            form = UserForm(request.POST)
+            if form.is_valid():
+                form.save(commit=True)
+                print('VALIDATED')
+                
+            else:
+                print('NOT VALIDATED')
     return render(request, 'profiles/index.html', {'form': form})
